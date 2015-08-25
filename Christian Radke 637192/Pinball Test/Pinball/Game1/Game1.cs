c319ec65d21707dc8ae82070b47983c9bacf172e
fdai4106 <christian.radke@informatik.hs-fulda.de>
2015-08-25 14:44:56 +0200

    Testprojekt eingefügt, Umstieg auf XNA bzw. Monogame. Erstes Einarbeiten in XNA/Monogame

---
+++ Christian Radke 637192/Pinball Test/Pinball/Game1/Game1.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Model model;
+        Matrix world, view, projection;
+        Vector2 resolution;
+        Vector3 moveVec;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            resolution.X = 1920.0f;
+            resolution.Y = 1080.0f;
+            graphics.PreferredBackBufferWidth = (int)resolution.X;
+            graphics.PreferredBackBufferHeight = (int)resolution.Y;
+
+            moveVec = Vector3.Zero;
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            view = Matrix.CreateLookAt(new Vector3(0, 0, 5), Vector3.Zero, Vector3.Up);
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, resolution.X / resolution.Y, 0.1f, 1000.0f);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            model = Content.Load<Model>("untitled");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+                moveVec.Z -= 0.1f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+                moveVec.X -= 0.1f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+                moveVec.Z += 0.1f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+                moveVec.X += 0.1f;
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            Matrix foo = Matrix.Identity * Matrix.CreateTranslation(new Vector3(-2.0f, 0.0f, 0.0f)) * Matrix.CreateRotationX(MathHelper.ToRadians(90.0f));
+            DrawSphere(foo);
+
+            foo = Matrix.Identity * Matrix.CreateTranslation(new Vector3(2.0f, 0.0f, 0.0f) + moveVec);
+            DrawSphere(foo);
+
+
+            base.Draw(gameTime);
+        }
+
+        private void DrawSphere(Matrix world)
+        {
+            this.world = world;
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach(BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = projection;
+
+                }
+
+                mesh.Draw();
+            }
+        }
+    }
+}
