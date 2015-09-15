020dbd3c1f6cad57feaf8fd10d098092a3889027
fdai4109 <kevin.neu@informatik.hs-fulda.de>
2015-09-15 18:48:34 +0200

    Pinball + Platte + Collision-Tests
    3D-Platte und 3D-Pinball eingesetzt und mit BoundingSpheres versehen.
    Collisions-Funktion grundlegend vorhanden, jedoch noch Fehler beim
    Einfallswinkel.
    
    Christian Radke, 637192
    Kevin Neu, 737193

--- Kevin Neu 737193/3DWorldTest/3DWorldTest/Game1.cs
+++ Kevin Neu 737193/3DWorldTest/3DWorldTest/Game1.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace _3DWorldTest
-{    
+{
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
@@ -13,12 +13,21 @@ namespace _3DWorldTest
         Vector3 camPosition;
         Matrix projectionMatrix;
         Matrix viewMatrix;
-        Matrix worldMatrix;
+        //Matrix worldMatrix;
+
+        Vector3 _platteLocation = new Vector3(0f, 0f, 0f);
+        Vector3 _pinballLocation = new Vector3(0f, 100f, 0f);
 
         Model _pinball;
+        Model _platte;
+
+        Vector3 _collisionPosition;
+        Vector3 _pinballLocationOLD = new Vector3(0f, 0f, 0f);
+
+        float _timer = 0f;
 
         //Orbit oder nicht Orbit?
-        bool orbit;  
+        bool orbit;
 
         public Game1()
         {
@@ -32,53 +41,69 @@ namespace _3DWorldTest
 
             //Kamera-Setup
             camTarget = new Vector3(0f, 0f, 0f);
-            camPosition = new Vector3(0f, 0f, -200f);
+            camPosition = new Vector3(0f, 100f, -180f);
 
             projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45f), GraphicsDevice.DisplayMode.AspectRatio, 1f, 1000f);
             viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, Vector3.Up);
-            worldMatrix = Matrix.CreateWorld(camTarget, Vector3.Forward, Vector3.Up);            
+            //worldMatrix = Matrix.CreateWorld(camTarget, Vector3.Forward, Vector3.Up);            
 
             _pinball = Content.Load<Model>("Pinball");
+            _platte = Content.Load<Model>("Platte");
 
         }
 
-        
+
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
         }
-        
+
         protected override void UnloadContent()
         {
-            
+
         }
 
         protected override void Update(GameTime gameTime)
         {
             //Game mit ESC schließen
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();            
+                Exit();
 
-            //Kamera-Bewegung
+            Matrix _pinballMatrix = Matrix.CreateTranslation(_pinballLocation);
+            Matrix _platteMatrix = Matrix.CreateTranslation(_platteLocation);
+
+            _timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (_timer >= 20f)
+            {
+                _pinballLocationOLD = _pinballLocation;
+            }
+
+            if (IsCollision(_pinball, _pinballMatrix, _platte, _platteMatrix))
+            {
+                _pinballLocation.Y += einfall(_pinballLocationOLD, _collisionPosition).Y;
+            }
+
+            //Kugel-Bewegung
             if (Keyboard.GetState().IsKeyDown(Keys.A))
             {
-                camPosition.X -= 1f;
-                camTarget.X -= 1f;
+                _pinballLocation.X += 1f;
+                //camTarget.X -= 1f;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.D))
             {
-                camPosition.X += 1f;
-                camTarget.X += 1f;
+                _pinballLocation.X -= 1f;
+                //camTarget.X += 1f;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.W))
             {
-                camPosition.Y -= 1f;
-                camTarget.Y -= 1f;
+                _pinballLocation.Y += 1f;
+                //camTarget.Y -= 1f;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.S))
             {
-                camPosition.Y += 1f;
-                camTarget.Y += 1f;
+                _pinballLocation.Y -= 1f;
+                //camTarget.Y += 1f;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.OemPlus))
             {
@@ -103,12 +128,43 @@ namespace _3DWorldTest
 
             base.Update(gameTime);
         }
-       
+
+        private Vector3 einfall(Vector3 a, Vector3 b)
+        {
+            return new Vector3 (b.X - a.X, b.Y - a.Y, b.Z - a.Z);
+        }
+
+        private bool IsCollision(Model model1, Matrix world1, Model model2, Matrix world2)
+        {
+            for (int meshIndex1 = 0; meshIndex1 < model1.Meshes.Count; meshIndex1++)
+            {
+                BoundingSphere sphere1 = model1.Meshes[meshIndex1].BoundingSphere;
+                sphere1 = sphere1.Transform(world1);
+
+                for (int meshIndex2 = 0; meshIndex2 < model2.Meshes.Count; meshIndex2++)
+                {
+                    BoundingSphere sphere2 = model2.Meshes[meshIndex2].BoundingSphere;
+                    sphere2 = sphere2.Transform(world2);
+
+                    if (sphere1.Intersects(sphere2))
+                        _collisionPosition = _pinballLocation;
+                        return true;
+                }
+            }
+            return false;
+        }
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            foreach (ModelMesh mesh in _pinball.Meshes)
+            Matrix _platteMatrix = Matrix.CreateTranslation(_platteLocation);
+            Matrix _pinballMatrix = Matrix.CreateTranslation(_pinballLocation);
+
+            DrawModel(_platte, _platteMatrix, viewMatrix, projectionMatrix);
+            DrawModel(_pinball, _pinballMatrix, viewMatrix, projectionMatrix);
+
+            /*foreach (ModelMesh mesh in _pinball.Meshes)
             {
                 foreach (BasicEffect effect in mesh.Effects)
                 {
@@ -120,7 +176,34 @@ namespace _3DWorldTest
                 mesh.Draw();
             }
 
+            foreach (ModelMesh mesh in _platte.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.AmbientLightColor = new Vector3(1f, 0, 0);
+                    effect.View = viewMatrix;
+                    effect.World = worldMatrix;
+                    effect.Projection = projectionMatrix;
+                }
+                mesh.Draw();
+            }*/
+
             base.Draw(gameTime);
+        }
+
+        private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.AmbientLightColor = new Vector3(1f, 0, 0);
+                    effect.View = view;
+                    effect.World = world;
+                    effect.Projection = projection;
+                }
+                mesh.Draw();
+            }
         }
     }
 }
