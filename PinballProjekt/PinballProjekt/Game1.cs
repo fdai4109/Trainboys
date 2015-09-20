e7272916c918fd1a081331cea0e80c694a13023d
fdai4109 <kevin.neu@informatik.hs-fulda.de>
2015-09-20 18:22:57 +0200

    Test...

---
+++ PinballProjekt/PinballProjekt/Game1.cs
@@ -0,0 +1,290 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PinballProjekt
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Vector3 camTarget;
+        Vector3 camPosition;
+        Matrix projectionMatrix;
+        Matrix viewMatrix;
+        //Matrix worldMatrix;
+
+        Vector3 _platteLocation = new Vector3(0f, 0f, 0f);
+        Vector3 _pinballLocation = new Vector3(0f, 0f, 0f);
+        Vector3 _triggerLocation = new Vector3(5f, 20f, -30f);
+
+        Model _pinball;
+        Model _platte;
+        Model _trigger;
+
+        Vector3 _collisionPosition;
+        Vector3 _pinballLocationOLD = new Vector3(0f, 0f, 0f);
+        float _streckeX;
+        float _streckeY;
+
+        float _timer = 0f;
+        float _velocityX = -0.3f;
+        float _velocityZ = -0.5f;
+
+        //Orbit oder nicht Orbit?
+        bool orbit;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            //Kamera-Setup
+            camTarget = new Vector3(0f, 0f, 0f);
+            camPosition = new Vector3(0f, 100f, -180f);
+
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45f), GraphicsDevice.DisplayMode.AspectRatio, 1f, 1000f);
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, Vector3.Up);
+            //worldMatrix = Matrix.CreateWorld(camTarget, Vector3.Forward, Vector3.Up);            
+
+            _pinball = Content.Load<Model>("Pinball");
+            _platte = Content.Load<Model>("Platte");
+            _trigger = Content.Load<Model>("Trigger");
+        }
+
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            //Game mit ESC schließen
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            Matrix _pinballMatrix = Matrix.CreateTranslation(_pinballLocation);
+            Matrix _platteMatrix = Matrix.CreateTranslation(_platteLocation);
+            Matrix _triggerMatrix = Matrix.CreateTranslation(_triggerLocation);
+           /* _streckeX = _pinballLocationOLD.X - _pinballLocation.X;
+            _streckeY = _pinballLocationOLD.Y - _pinballLocation.Y;
+
+            _velocityX = _streckeX / _timer;
+            _velocityY = _streckeY / _timer;*/
+
+            _timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            if (_timer >= 200f)
+            {
+                _pinballLocationOLD = _pinballLocation;
+                /*_velocityX *= 0.9f;
+                _velocityY *= 0.9f;*/
+                _timer = 0f;
+            }
+
+            if(EdgeCollisionObenUnten())
+            {
+                //_pinballLocation += einfall(_pinballLocationOLD, _collisionPosition);
+                _velocityX *= 1;
+                _velocityZ *= -1;
+                System.Diagnostics.Debug.WriteLine("Collision");
+            }
+
+            if(EdgeCollisionRechtsLinks())
+            {
+                _velocityX *= -1;
+                _velocityZ *= 1;
+            }
+
+            /*if (IsCollision(_pinball, _pinballMatrix, _platte, _platteMatrix))
+            {
+                _pinballLocation += einfall(_pinballLocationOLD, _collisionPosition);
+                _velocityX *= 1;
+                _velocityY *= -1;
+                System.Diagnostics.Debug.WriteLine("ISCOLLI");
+            }*/
+
+            //Kugel-Bewegung
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                _pinballLocation.X += 1f;
+                //camTarget.X -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                _pinballLocation.X -= 1f;
+                //camTarget.X += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                _pinballLocation.Y += 1f;
+                //camTarget.Y -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                _pinballLocation.Y -= 1f;
+                //camTarget.Y += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemPlus))
+            {
+                camPosition.Z += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemMinus))
+            {
+                camPosition.Z -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                orbit = !orbit;
+            }
+
+            //Kamera dreht sich automatisch um Target, Orbit
+            if (orbit)
+            {
+                Matrix rotationMatrix = Matrix.CreateRotationY(MathHelper.ToRadians(1f));
+                camPosition = Vector3.Transform(camPosition, rotationMatrix);
+            }
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, Vector3.Up);
+
+            System.Diagnostics.Debug.WriteLine(_pinballLocation.Y + " - Aktuell Y");
+            System.Diagnostics.Debug.WriteLine(_pinballLocationOLD.Y + " - Alt Y");
+            System.Diagnostics.Debug.WriteLine(_pinballLocation.X + " - Aktuell X");
+            System.Diagnostics.Debug.WriteLine(_pinballLocationOLD.X + " - Alt X");
+            System.Diagnostics.Debug.WriteLine(_pinballLocation.Z + " - AKtuell Z");
+            System.Diagnostics.Debug.WriteLine(_pinballLocationOLD.Z + "- Alt Z ");
+
+            System.Diagnostics.Debug.WriteLine(_velocityX + "- Vel X");
+            System.Diagnostics.Debug.WriteLine(_velocityZ + "- Vel Y");
+
+            _pinballLocation.Z += _velocityZ;
+            _pinballLocation.X += _velocityX;
+
+            base.Update(gameTime);
+        }
+
+        private Vector3 einfall(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z)*5;
+        }
+
+        private bool IsCollision(Model model1, Matrix world1, Model model2, Matrix world2)
+        {
+            for (int meshIndex1 = 0; meshIndex1 < model1.Meshes.Count; meshIndex1++)
+            {
+                BoundingSphere sphere1 = model1.Meshes[meshIndex1].BoundingSphere;
+                sphere1 = sphere1.Transform(world1);
+
+                for (int meshIndex2 = 0; meshIndex2 < model2.Meshes.Count; meshIndex2++)
+                {
+                    BoundingSphere sphere2 = model2.Meshes[meshIndex2].BoundingSphere;
+                    sphere2 = sphere2.Transform(world2);
+
+                    if (sphere1.Intersects(sphere2))
+                    {
+                        _collisionPosition = _pinballLocation;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool EdgeCollisionObenUnten()
+        {
+            
+             if(_pinballLocation.Z >= 48)
+            {
+                _collisionPosition = _pinballLocation;
+                return true;
+            }
+            else if(_pinballLocation.Z <= -48)
+            {
+                _collisionPosition = _pinballLocation;
+                return true;
+            }
+            
+            
+                return false;
+        }
+
+        private bool EdgeCollisionRechtsLinks()
+        {
+            if (_pinballLocation.X >= 23)
+            {
+                _collisionPosition = _pinballLocation;
+                return true;
+            }
+            else if (_pinballLocation.X <= -23)
+            {
+                _collisionPosition = _pinballLocation;
+                return true;
+            }
+            return false;
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            Matrix _platteMatrix = Matrix.CreateTranslation(_platteLocation);
+            Matrix _pinballMatrix = Matrix.CreateTranslation(_pinballLocation);
+            Matrix _triggerMatrix = Matrix.CreateTranslation(_triggerLocation);
+
+            DrawModel(_platte, _platteMatrix, viewMatrix, projectionMatrix);
+            DrawModel(_pinball, _pinballMatrix, viewMatrix, projectionMatrix);
+            DrawModel(_trigger, _triggerMatrix, viewMatrix, projectionMatrix);
+            /*foreach (ModelMesh mesh in _pinball.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.AmbientLightColor = new Vector3(1f, 0, 0);
+                    effect.View = viewMatrix;
+                    effect.World = worldMatrix;
+                    effect.Projection = projectionMatrix;
+                }
+                mesh.Draw();
+            }
+
+            foreach (ModelMesh mesh in _platte.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.AmbientLightColor = new Vector3(1f, 0, 0);
+                    effect.View = viewMatrix;
+                    effect.World = worldMatrix;
+                    effect.Projection = projectionMatrix;
+                }
+                mesh.Draw();
+            }*/
+
+            base.Draw(gameTime);
+        }
+
+        private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.AmbientLightColor = new Vector3(1f, 0, 0);
+                    effect.View = view;
+                    effect.World = world;
+                    effect.Projection = projection;
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
